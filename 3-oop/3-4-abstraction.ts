d8888e558f4af8efc2090a648b72775d1bbe27e0
dev-bakery <rimming2255@naver.com>
2023-07-19 22:33:05 +0900

    추상화란 무엇일까

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -7,13 +7,28 @@
   interface CoffeeMaker {
     makeCoffee(shots: number): CoffeeCup;
   }
-  class CoffeeMachine implements CoffeeMaker {
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     // CoffeeMaker 인터페이스 규격에 따라 CoffeeMachine을 구현해야함
-    static BEANS_GRAMM_PER_SHOT: number = 7;
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
     //변하지 않는 데이터는 static을 선언해서 class level로 지정함으로써 new CoffeeMachine() 할때 마다 BEANS_GRAMM_PER_SHOT이 생성되는 것을 막음
-    coffeeBeans: number = 0; // instance level
-    constructor(coffeeBeans: number) {
+    private coffeeBeans: number = 0; // instance level
+    private constructor(coffeeBeans: number) {
       this.coffeeBeans = coffeeBeans;
+    }
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number): void {
+      this.coffeeBeans = beans;
+    }
+    clean(): void {
+      console.log("cleaning the machine");
     }
 
     private grindBeans(shots: number) {
@@ -41,7 +56,27 @@
     }
   }
 
-  const maker = new CoffeeMachine(50); //클래스를 생성할때 커피콩 갯수를 늘리고 시작!
-  const coffee = maker.makeCoffee(2);
-  console.log(coffee);
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(4);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(59);
+      this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(100);
+  const amateur = new AmateurUser(maker);
+  amateur.makeCoffee();
+  const pro = new ProBarista(maker);
+  pro.makeCoffee();
 }
