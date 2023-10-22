1838791868b761fa7454d5f4ed26f92292de88b8
dev-bakery <rimming2255@naver.com>
2023-10-22 14:43:58 +0900

    컴포넌트 구조 완성

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,22 +1,27 @@
+import { Component } from "./components/page/component.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
-import { PageComponent } from "./components/page/page.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
+    const video = new VideoComponent("영상", "https://youtu.be/K3-jG52XwuQ");
+    this.page.addChild(video);
+
     const image = new ImageComponent("첫번째 이미지", "https://dummyimage.com/600x400/000/fff");
-    image.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
 
     const note = new NoteComponent("노트 1", "이게 바로 첫번째 노트야");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponent("할일 1", "장보기");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
   }
 }
 
--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -1,15 +1,26 @@
 import { BaseComponent } from "../component.js";
 
-export class ImageComponent extends BaseComponent<HTMLElement> {
+export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
-            
-          </section>`);
-    const imageElement = this.element.querySelector(".image__thumbnail")! as HTMLImageElement;
-    imageElement.src = url;
-    imageElement.alt = title;
+      <div class="video__player">
+        <iframe class="video__iframe"></iframe>
+      </div>
+      <p class="video__title"></p>
+    </section>`);
+    const iframeElement = this.element.querySelector(".video__iframe")! as HTMLIFrameElement;
+    iframeElement.src = this.convertToEmbeddedURL(url);
 
-    const titleElement = this.element.querySelector(".image__title")! as HTMLParagraphElement;
+    const titleElement = this.element.querySelector(".video__title")! as HTMLParagraphElement;
     titleElement.textContent = title;
+  }
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
